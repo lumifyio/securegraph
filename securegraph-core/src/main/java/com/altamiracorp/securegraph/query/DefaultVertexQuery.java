74c6d1982dccc85dbc8835fd3b3b81da1d3613b0
Joe Ferner <joe@fernsroth.com>
2014-01-02 13:11:13 -0500

    implement DefaultVertexQuery edges

--- securegraph-core/src/main/java/com/altamiracorp/securegraph/query/DefaultVertexQuery.java
+++ securegraph-core/src/main/java/com/altamiracorp/securegraph/query/DefaultVertexQuery.java
@@ -4,6 +4,7 @@ import com.altamiracorp.securegraph.Authorizations;
 import com.altamiracorp.securegraph.Edge;
 import com.altamiracorp.securegraph.Graph;
 import com.altamiracorp.securegraph.Vertex;
+import com.altamiracorp.securegraph.util.FilterIterable;
 
 public class DefaultVertexQuery extends VertexQueryBase implements VertexQuery {
     public DefaultVertexQuery(Graph graph, Authorizations authorizations, Vertex sourceVertex) {
@@ -12,6 +13,17 @@ public class DefaultVertexQuery extends VertexQueryBase implements VertexQuery {
 
     @Override
     public Iterable<Edge> edges() {
-        return null;
+        return new FilterIterable<Edge>(getGraph().getEdges(getParameters().getAuthorizations())) {
+            @Override
+            protected boolean isIncluded(Edge edge) {
+                if (edge.getOutVertexId().equals(getSourceVertex().getId())) {
+                    return true;
+                }
+                if (edge.getInVertexId().equals(getSourceVertex().getId())) {
+                    return true;
+                }
+                return false;
+            }
+        };
     }
 }
