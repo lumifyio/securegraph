4e19b4349b8d208cd9914efe286f305d34bffa36
David Singley <dsingley@nearinfinity.com>
2014-04-18 13:46:06 -0400

    scripts for managing sneakernet

---
+++ bin/unbundle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+
+(
+  cd ${DIR}/../..
+  bundle_filename=$(ls secure-graph.*.bundle | tail -1)
+  unlink secure-graph.bundle
+  ln -s ${bundle_filename} secure-graph.bundle
+
+  cd ${DIR}/..
+  git pull
+  echo "updated from ${bundle_filename}"
+)
